ab9072b27a19c9ea9465485f7a801738ad51e4f1
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2024-09-25 14:18:06 +0530

    array

---
+++ Arrays/src/mergeSortedArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class mergeSortedArray {
+    //88. Merge Sorted Array
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m + n - 1;
+        m--;
+        n--;
+
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] > nums2[n]) {
+                nums1[i--] = nums1[m--];
+            } else {
+                nums1[i--] = nums2[n--];
+            }
+        }
+
+        while (n >= 0) {
+            nums1[i--] = nums2[n--];
+        }
+
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr1[] = {1,2,3,0,0,0};
+        int arr2[] = {2,5,6};
+        int m = 3 , n=3;
+        merge(arr1,m,arr2,n);
+    }
+}
