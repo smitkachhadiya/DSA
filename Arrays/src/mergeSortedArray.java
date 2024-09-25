c71db216f1767c381752750e3971dbb5c39503bc
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2024-09-25 14:25:24 +0530

    array

--- Arrays/src/mergeSortedArray.java
+++ Arrays/src/mergeSortedArray.java
@@ -8,6 +8,7 @@ public class mergeSortedArray {
         int i = m + n - 1;
         m--;
         n--;
+
         while (m >= 0 && n >= 0) {
             if (nums1[m] > nums2[n]) {
                 nums1[i--] = nums1[m--];
