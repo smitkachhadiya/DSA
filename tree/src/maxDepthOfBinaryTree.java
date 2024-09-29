8df7f172d1cdc75cc973d3fe74d24577620f483e
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2024-09-29 18:25:14 +0530

    tree

---
+++ tree/src/maxDepthOfBinaryTree.java
@@ -0,0 +1,35 @@
+public class maxDepthOfBinaryTree {
+
+    private static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    //104. Maximum Depth of Binary Tree
+    public static int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left , right) +1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(maxDepth(root));
+    }
+}
