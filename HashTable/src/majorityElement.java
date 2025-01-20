119583d60346ca91e1c120577868032001ea7a4f
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2025-01-20 17:52:37 +0530

    HashTable

---
+++ HashTable/src/majorityElement.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class majorityElement {
+
+    //
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer , Integer> count = new HashMap<>();
+        int majority = nums.length/2;
+
+        for(int n : nums){
+            count.put(n , count.getOrDefault(n,0)+1);
+            if (count.get(n) >= majority){
+                return n;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        majorityElement me = new majorityElement();
+        int[] nums1 = {3, 2, 3 ,1};
+        System.out.println(me.majorityElement(nums1));
+    }
+}
