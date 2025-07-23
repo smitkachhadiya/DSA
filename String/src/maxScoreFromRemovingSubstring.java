da3dd0f5a61b74456278f8f76ab34a5e2af24964
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2025-07-23 20:17:58 +0530

    1717. Maximum Score From Removing Substrings

---
+++ String/src/maxScoreFromRemovingSubstring.java
@@ -0,0 +1,38 @@
+package DSA.Astring;
+
+public class maxScoreFromRemovingSubstring {
+
+    //  1717. Maximum Score From Removing Substrings
+
+    public static int maximumGain(String s, int x, int y) {
+        char[] chars = s.toCharArray();
+        int total = 0, a = 0, b = 0;
+
+        char first = x > y ? 'a' : 'b', second = x > y ? 'b' : 'a';
+        int max = Math.max(x, y), min = Math.min(x, y);
+
+        for (char c : chars) {
+            if (c == first) {
+                a++;
+            } else if (c == second) {
+                if (a > 0) {
+                    a--;
+                    total += max;
+                } else {
+                    b++;
+                }
+            } else {
+                total += Math.min(a, b) * min;
+                a = b = 0;
+            }
+        }
+
+        return total + Math.min(a, b) * min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maximumGain("cdbcbbaaabab",4,5));
+        System.out.println(maximumGain("aabbaaxybbaabb",5,4));
+        System.out.println(maximumGain("aabbaaxybbaabb",5,8));
+    }
+}
