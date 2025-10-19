b03e82df8a0b78ce91944ff0d106e7c64dfb2497
smitkachhadiya <smitkachhadiya221@gmail.com>
2025-10-19 21:14:43 +0530

    98. Validate Binary Search Tree

---
+++ tree/src/validateBinarySearchTree.java
@@ -0,0 +1,17 @@
+public class validateBinarySearchTree {
+    
+    //  98. Validate Binary Search Tree
+
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean valid(TreeNode node, long minimum, long maximum) {
+        if (node == null) return true;
+
+        if (!(node.val > minimum && node.val < maximum)) {
+            return false;
+        }
+
+        return valid(node.left, minimum, node.val) && valid(node.right, node.val, maximum);
+    }
+}
