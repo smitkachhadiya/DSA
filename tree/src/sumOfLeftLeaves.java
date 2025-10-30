aa935fd91b7d859e5279795f5eca1191adb8ee49
smitkachhadiya <smitkachhadiya221@gmail.com>
2025-10-30 17:36:01 +0530

    404. Sum of Left Leaves

---
+++ tree/src/sumOfLeftLeaves.java
@@ -0,0 +1,20 @@
+public class sumOfLeftLeaves {
+
+    //  404. Sum of Left Leaves
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        return dfs(root, false);
+    }
+
+    private int dfs(TreeNode node, boolean isLeft) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return isLeft ? node.val : 0;
+        }
+        int leftSum = dfs(node.left, true);
+        int rightSum = dfs(node.right, false);
+        return leftSum + rightSum;
+    }
+}
