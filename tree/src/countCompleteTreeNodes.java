b6ecd63a2d4ed3a5207f5272f9e1c2f93908a97a
smitkachhadiya <smitkachhadiya221@gmail.com>
2025-10-30 17:28:27 +0530

    222. Count Complete Tree Nodes

---
+++ tree/src/countCompleteTreeNodes.java
@@ -0,0 +1,10 @@
+public class countCompleteTreeNodes {
+
+    //  222. Count Complete Tree Nodes
+
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
