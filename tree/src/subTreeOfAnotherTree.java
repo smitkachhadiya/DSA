108dd5f654108ac5b23e719d5df95e1837b84eb8
smitkachhadiya <smitkachhadiya221@gmail.com>
2026-02-18 18:08:49 +0530

    572. Subtree of Another Tree

---
+++ tree/src/subTreeOfAnotherTree.java
@@ -0,0 +1,19 @@
+public class subTreeOfAnotherTree {
+
+    //  572. Subtree of Another Tree
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) return false;
+        if(isSame(root,subRoot)) return true;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right,subRoot);
+    }
+
+    public boolean isSame(TreeNode t1, TreeNode t2){
+        if(t1 == null && t2 == null) return true;
+        if(t1 == null || t2 == null) return false;
+
+        if(t1.val != t2.val) return false;
+        return isSame(t1.left,t2.left) && isSame(t1.right,t2.right);
+    }
+}
