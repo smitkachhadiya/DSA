f7eb294ec4cb316de060d0c0f8c468e8b6df8dc1
smitkachhadiya <smitkachhadiya221@gmail.com>
2026-02-18 17:47:52 +0530

    543. Diameter of Binary Tree

---
+++ tree/src/diameterOfBinaryTree.java
@@ -0,0 +1,24 @@
+public class diameterOfBinaryTree {
+
+    //  543. Diameter of Binary Tree
+
+    int res = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+
+        res = Math.max(res, l + r);
+
+        return 1 + Math.max(l, r);
+    }
+}
