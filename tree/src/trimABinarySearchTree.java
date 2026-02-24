4056f8d0d39b3f930fb4deb826bba75799ae78b5
smitkachhadiya <smitkachhadiya221@gmail.com>
2026-02-24 16:49:59 +0530

    669. Trim a Binary Search Tree

---
+++ tree/src/trimABinarySearchTree.java
@@ -0,0 +1,18 @@
+public class trimABinarySearchTree {
+
+    //  669. Trim a Binary Search Tree
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return null;
+
+        if (root.val >= low && root.val <= high){
+            root.left = trimBST(root.left,low,high);
+            root.right = trimBST(root.right,low,high);
+        } else if(root.val < low){
+            root = trimBST(root.right,low,high);
+        } else if (root.val > high) {
+            root = trimBST(root.left,low,high);
+        }
+        return root;
+    }
+}
