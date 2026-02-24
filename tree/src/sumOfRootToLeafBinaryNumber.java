4240c9d44e37609be7dbe02e5db595c2edc43e4f
smitkachhadiya <smitkachhadiya221@gmail.com>
2026-02-24 16:41:16 +0530

    1022. Sum of Root To Leaf Binary Numbers

---
+++ tree/src/sumOfRootToLeafBinaryNumber.java
@@ -0,0 +1,20 @@
+public class sumOfRootToLeafBinaryNumber {
+
+    //  1022. Sum of Root To Leaf Binary Numbers
+
+    public int sumRootToLeaf(TreeNode root) {
+        return helper(root,0);
+    }
+
+    public int helper(TreeNode node , int sum){
+        if(node == null) return 0;
+
+        sum = sum * 2 + node.val;
+
+        if(node.left == null && node.right == null){
+            return sum;
+        }
+
+        return helper(node.left,sum) + helper(node.right,sum);
+    }
+}
