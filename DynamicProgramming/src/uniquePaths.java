54d59da6dfabfe9ea3da91bec0fd1ae99ca2f8d8
smitkachhadiya <smitkachhadiya221@gmail.com>
2026-07-05 22:39:12 +0530

    62. Unique Paths

---
+++ DynamicProgramming/src/uniquePaths.java
@@ -0,0 +1,27 @@
+public class uniquePaths {
+
+    //  62. Unique Paths
+
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dp[i][n-1] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[m-1][j] = 1;
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3,7));
+        System.out.println(uniquePaths(3,2));
+    }
+}
